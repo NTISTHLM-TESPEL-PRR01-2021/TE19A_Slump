c3927d4ddeb95ae6cde5fb69c6293d0ad8b11583
Mikael <mikael.bergstrom@ga.ntig.se>
2020-10-08 23:22:47 +0200

    Initial commit

---
+++ Slump/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Slump
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Random generator = new Random();
+      Random secondGenerator = new Random();
+
+
+      while (true)
+      {
+        int tal = generator.Next(-100, 100);
+        // int tal2 = secondGenerator.Next(50, 101);
+
+        // int sum = (tal + tal2) / 2;
+
+        Console.WriteLine(tal);
+        // Console.WriteLine("Från tvåan: " + tal2);
+        Console.ReadLine();
+      }
+
+
+
+
+    }
+  }
+}
